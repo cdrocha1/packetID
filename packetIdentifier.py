39a1e303371899b611b1717b9b834b7026a51b8c
Ricardo Mendez <rmendez13@miners.utep.edu>
2022-04-17 22:17:49 +0000

    Id source of node

--- packetIdentifier.py
+++ packetIdentifier.py
@@ -11,9 +11,7 @@
 from pprint import pprint
 
 import cantools
-
 import can
-
 
 db = cantools.database.load_file('CSS-Electronics-SAE-J1939-DEMO.dbc')
 print(db.messages)
@@ -23,19 +21,50 @@ print('em signals: ', example_message.signals)
 print('em id:', example_message.frame_id)
 
 
-
-
-# db.decode_message(int(message[2], 16), bytes.fromhex(message[3]))
 packetDict = {}
+source_list = []
+
 f = open("packets.txt", "r")
 f.readline()
+
+#create packet dictionary{key = indicator, value = data}
 for x in f:
     newline = (x.split(";"))
-    print(newline)
-
     packetDict[newline[2]] = newline[-1]
 
-i = 0
+#decode source of indicators (assume first 2 values of string indicate region)
 for key in packetDict.keys():
-    print(i, ': ', key)
-    i+=1
+    source = key[:2]
+    source_list.append(source)
+
+#function to find each unique source in source_list
+def name_list(source_list):
+    unique_sources = []
+    for source in source_list:
+        if source not in unique_sources:
+            unique_sources.append(source)
+    return unique_sources
+
+def print_nodes(node_id, packet_dictionary):
+    for key in packet_dictionary.keys():
+        if node_id == str(key[:2]):
+            print(key)
+            print(packet_dictionary.get(key))
+
+
+#terminal interface
+interface = 1
+
+sources = name_list(source_list)
+print('Select what node you would like to access: ', sources)
+responce = raw_input()
+print('You Selected: ', responce)
+print_nodes(responce, packetDict)
+
+
+    
+
+
+
+
+
