ad62ecf6ee8dbe5f46b7fe2f55a0c37a8d16ded9
cdrocha <cdrocha@miners.utep.edu>
2022-10-01 13:03:09 -0600

    Fixed dictionary bug

--- PacketManager.py
+++ PacketManager.py
@@ -3,9 +3,9 @@ import csv
 class PacketManager:
     def createPacketDict(packets_file):
         packet_dict = {}
-        
+        fieldNames = ['Timestamp', 'Type', 'ID', 'Data']
         with open(packets_file, newline='\n') as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.DictReader(csvfile, fieldnames=fieldNames)
             for row in reader:
                 print(row)
         return reader
