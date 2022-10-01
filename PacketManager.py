597e3ba2ce6654750967da0de6ea1850a4f4f502
cdrocha <cdrocha@miners.utep.edu>
2022-10-01 13:16:05 -0600

    fixed Dict

--- PacketManager.py
+++ PacketManager.py
@@ -3,13 +3,19 @@ import csv
 class PacketManager:
     def createPacketDict(packets_file):
         packet_dict = {}
+        packets = []
         fieldNames = ['Timestamp', 'Type', 'ID', 'Data']
         with open(packets_file, newline='\n') as csvfile:
-            reader = csv.DictReader(csvfile, fieldnames=fieldNames)
+            reader = csv.DictReader(csvfile, fieldnames=fieldNames, delimiter=';')
+            i = 0
             for row in reader:
-                print(row)
-        return reader
+                packets.append(row)
 
-PacketManager.createPacketDict('packets.csv')
+        return packets
+
+packetCSV = PacketManager.createPacketDict('packets1.csv')
+
+for x in packetCSV:
+    print(x)
 # out : {'Timestamp;Type;ID;Data': '11T082557132;1;cf00203;cde0270004b827ff'}
 # out : {'Timestamp;Type;ID;Data': '11T082557132;1;18fe592f;ffffffffe37cdd7c'}
